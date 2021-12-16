94b4f553fc4953e8f441d27a2c356ded72b71223
shra1dhar <shraonedhar@gmail.com>
2021-12-16 23:31:12 +0530

    Updating semi structured logs logic

--- exercism/semi-structured-logs.rs
+++ exercism/semi-structured-logs.rs
@@ -1,20 +1,36 @@
-/// various log levels
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
 }
-/// primary function for emitting logs
+
 pub fn log(level: LogLevel, message: &str) -> String {
-    format!("[{}]: {}", level, message);
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message)
+    }
 }
+
 pub fn info(message: &str) -> String {
-    log(LogLevel::Info, message);
+    let mut owned_string: String = "[INFO]: ".to_owned();
+    owned_string.push_str(message);
+    return owned_string;
 }
+
 pub fn warn(message: &str) -> String {
-    log(LogLevel::WARNING, message)
+    let mut owned_string: String = "[WARNING]: ".to_owned();
+    owned_string.push_str(message);
+    return owned_string;
 }
+
 pub fn error(message: &str) -> String {
-    log(LogLevel::Error, message)
+    let mut owned_string: String = "[ERROR]: ".to_owned();
+    owned_string.push_str(message);
+    return owned_string;
 }
