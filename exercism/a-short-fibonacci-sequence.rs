ebb110ce7eacdc161f1118195f70b53f5b2a396f
shra1dhar <shraonedhar@gmail.com>
2021-12-17 23:03:20 +0530

    A short fibonacci sequence :crab:

---
+++ exercism/a-short-fibonacci-sequence.rs
@@ -0,0 +1,22 @@
+/// Create an empty vector
+pub fn create_empty() -> Vec<u8> {
+    let v: Vec<u8> = Vec::new();
+    return v;
+}
+
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+pub fn create_buffer(count: usize) -> Vec<u8> {
+    let v: Vec<u8> = vec![0; count];
+    return v;
+}
+
+/// Create a vector containing the first five elements of the Fibonacci sequence.
+///
+/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
+pub fn fibonacci() -> Vec<u8> {
+    let v = vec![1, 1, 2, 3, 5];
+    return v;
+}
