add745be117a668803e5bf933b4d79c31352758d
shra1dhar <shraonedhar@gmail.com>
2021-12-21 23:57:28 +0530

    Resistor colors :crab:

---
+++ exercism/resistor-color.rs
@@ -0,0 +1,31 @@
+use enum_iterator::IntoEnumIterator;
+use int_enum::{IntEnum, IntEnumError};
+
+#[derive(Debug, PartialEq)]
+pub enum ResistorColor {
+    Black=0,
+    Blue=1,
+    Brown=2,
+    Green=3,
+    Grey=4,
+    Orange=5,
+    Red=6,
+    Violet=8,
+    White=9,
+    Yellow=10,
+}
+
+pub fn color_to_value(_color: ResistorColor) -> usize {
+    _color as usize
+}
+
+pub fn value_to_color_string(value: usize) -> String {
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string(),
+    }
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    ResistorColor::into_enum_iter().collect()
+}
