b6dff9faededaa00bfb0d99e5c1411e4f3c96c24
shra1dhar <shraonedhar@gmail.com>
2021-12-30 23:56:02 +0530

    File reading :crab:

--- rust-cli/src/main.rs
+++ rust-cli/src/main.rs
@@ -22,4 +22,13 @@ fn main() {
             println!("{}", line);
         }
     }
+
+    let result = std::fs::read_to_string("test.txt");
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => {
+            panic!("Can't deal with {}, just exit here", error);
+        }
+    };
+    println!("file content: {}", content);
 }
